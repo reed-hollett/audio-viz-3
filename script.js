d493552d12632f90f9bf3a8e46cfb70b2c404f60
reed-hollett <149107702+reed-hollett@users.noreply.github.com>
2023-11-03 16:16:51 -0400

    new waveform with history

--- script.js
+++ script.js
@@ -9,42 +9,50 @@ const source = audioContext.createMediaElementSource(audio);
 source.connect(analyser);
 analyser.connect(audioContext.destination);
 
-analyser.fftSize = 2048; // Using a larger FFT size for waveform data
+analyser.fftSize = 2048;
 const bufferLength = analyser.fftSize;
-const dataArray = new Float32Array(bufferLength); // Using Float32Array for waveform data
+const dataArray = new Float32Array(bufferLength);
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const history = []; // To store past waveforms
+
 function draw() {
     requestAnimationFrame(draw);
 
-    analyser.getFloatTimeDomainData(dataArray); // Get waveform data
+    analyser.getFloatTimeDomainData(dataArray);
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    // Push current dataArray into history
+    history.push([...dataArray]);
+    if (history.length > canvas.width / 2) history.shift();
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.lineWidth = 2;
-    ctx.strokeStyle = '#00FF00'; // Green color for the waveform
+    ctx.strokeStyle = '#00FF00';
 
-    ctx.beginPath();
-    const sliceWidth = canvas.width * 1.0 / bufferLength;
-    let x = 0;
+    for (let h = 0; h < history.length; h++) {
+        ctx.beginPath();
+        const sliceWidth = canvas.width * 1.0 / bufferLength;
+        let x = canvas.width / 2 + h - history.length; // Start from the center and scroll to the right
 
-    for (let i = 0; i < bufferLength; i++) {
-        const v = dataArray[i] * 200; // Amplifying the waveform for better visibility
-        const y = v + canvas.height / 2; // Centering the waveform vertically
+        for (let i = 0; i < bufferLength; i++) {
+            const v = history[h][i] * 200;
+            const y = v + canvas.height / 2;
 
-        if (i === 0) {
-            ctx.moveTo(x, y);
-        } else {
-            ctx.lineTo(x, y);
+            if (i === 0) {
+                ctx.moveTo(x, y);
+            } else {
+                ctx.lineTo(x, y);
+            }
+            x += sliceWidth;
         }
-        x += sliceWidth;
-    }
 
-    ctx.lineTo(canvas.width, canvas.height / 2);
-    ctx.stroke();
+        ctx.lineTo(canvas.width, canvas.height / 2);
+        ctx.stroke();
+    }
 }
 
 playButton.addEventListener('click', function() {
