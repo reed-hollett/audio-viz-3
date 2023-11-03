aa91aefd4499143f1d41a3a46ad4b3bd07981bf9
reed-hollett <149107702+reed-hollett@users.noreply.github.com>
2023-11-03 16:52:43 -0400

    big styling changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const audio = document.getElementById('audio');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const playButton = document.getElementById('playButton');
+const timeCounter = document.getElementById('timeCounter');
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const analyser = audioContext.createAnalyser();
@@ -13,10 +14,10 @@ analyser.fftSize = 256;
 const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = document.querySelector('.container').clientWidth - 32; // Adjusted for padding
+canvas.height = window.innerHeight * 0.8;
 
-const history = []; // To store past volume levels
+const history = [];
 
 function draw() {
     requestAnimationFrame(draw);
@@ -25,18 +26,18 @@ function draw() {
 
     const averageVolume = dataArray.reduce((a, b) => a + b) / dataArray.length;
     history.push(averageVolume);
-    if (history.length > canvas.width * 0.65) history.shift(); // Adjusted to start shifting at 65%
+    if (history.length > canvas.width * 0.65) history.shift();
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.lineWidth = 2;
-    ctx.strokeStyle = '#00FF00';
+    ctx.strokeStyle = '#FD8775'; // Waveform color
 
     for (let h = 0; h < history.length; h++) {
-        const x = h; // Start from the left
-        const height = (history[h] / 256) * canvas.height; // Scaling to the full canvas height
-        const y = canvas.height / 2 - height / 2; // Centering vertically
+        const x = h;
+        const height = (history[h] / 256) * canvas.height;
+        const y = canvas.height / 2 - height / 2;
 
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -50,6 +51,13 @@ playButton.addEventListener('click', function() {
         audio.play();
         draw();
     });
+});
+
+audio.addEventListener('timeupdate', function() {
+    const currentTime = audio.currentTime;
+    const minutes = Math.floor(currentTime / 60).toString().padStart(2, '0');
+    const seconds = (currentTime % 60).toFixed(1).padStart(4, '0');
+    timeCounter.textContent = `• ${minutes}:${seconds}`;
 });
 
 audio.addEventListener('error', function(e) {
